da95894ebcfe805302e5b5f45066929790af8ba9
Nhi Danis <a.nhi1001@gmail.com>
2020-12-08 13:37:21 -0800

    commit employee inquirer questions

--- Develop/app.js
+++ Develop/app.js
@@ -11,3 +11,57 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
+const outputArr = [];
+const questions = [
+    {
+        type: 'list' ,
+        name: 'role',
+        message: "What is the employee role?",
+        choices: ["Intern", "Engineer", "Manager"]
+    },
+
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the employee's full name?",
+    },
+
+    {
+        type: 'input',
+        name: 'email' ,
+        message: "What is the employee's emaill address?",
+    },
+
+    {
+        type: 'input',
+        name: 'id' ,
+        message: "What is the employee's ID?" ,
+    },
+
+    {
+        type: 'input',
+        name: 'school',
+        message: "Please enter the school that the intern attend?",
+        when: function(answers){
+            return answers.role === "Intern";
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'github',
+        message: "Please enter the Github usename?",
+        when: function(answers){
+            return answers.role === "Engineer";
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'officenumber',
+        message: "Please enter the office numbers?",
+        when: function(answers){
+            return answers.role === "Manager";
+        }
+    },
+]
