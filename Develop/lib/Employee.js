70ad04ac9922a3f51b1644d156b846552ba5a1b1
Nhi Danis <a.nhi1001@gmail.com>
2020-12-08 13:01:29 -0800

    commit export the Employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor (name, id, email) {
+        this.name = name;
+        this.id = id; 
+        this.enmail = email;
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getId() {
+        return this.id
+    }
+
+    getEmail() {
+        this.email
+    }
+}
+
+module.exports = Employee
